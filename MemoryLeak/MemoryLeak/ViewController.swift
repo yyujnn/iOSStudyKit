c74d1c9ba2c09b1ce6d70a4c733e8e57a5a90c6e
정유진 <yujin3504@gmail.com>
2025-04-25 19:18:06 +0900

    setting - init project

---
+++ MemoryLeak/MemoryLeak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryLeak
+//
+//  Created by 정유진 on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
