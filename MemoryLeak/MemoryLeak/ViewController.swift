a52c2bfec812a0edd6618d2c85a09d81e40fea3e
정유진 <yujin3504@gmail.com>
2025-04-28 15:31:32 +0900

    feat: 메모리릭 코드 작성

--- MemoryLeak/MemoryLeak/ViewController.swift
+++ MemoryLeak/MemoryLeak/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ClassA {
+    var x: ClassB?
+}
 
+class ClassB {
+    var x: ClassA?
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+    
+    // 버튼 클릭 시 메모리 누수가 나도록 코드 작성
+    @IBAction func buttonTapped(_ sender: Any) {
+        let classA = ClassA()
+        let classB = ClassB()
+        
+        // ClassA ↔ ClassB 가 서로를 순환 참조하도록 설정
+        classA.x = classB
+        classB.x = classA
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
