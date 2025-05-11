7a6ddd395db521323f3d4c8395f761408dc6141d
정유진 <yujin3504@gmail.com>
2025-05-11 16:53:56 +0900

    study: RxSwift 실습 및 정리

--- RxSwiftStudy/RxSwift.md
+++ RxSwiftStudy/RxSwift.md
@@ -10,3 +10,34 @@
 **RxSwift**
 - ReactiveX 의 프로그래밍 철학을 따르기 때문에, 같은 철학을 따르는 `RxJava`, `RxPython`, `RxJS` 등 다른 언어를 사용하는 개발자들과 원활한 대화할 수 있다.
 - 특히 안드로이드 개발자도 `RxJava` 로 안드로이드 앱 개발을 한다면, 중요한 로직등을 싱크를 맞춰서 함께 설계할 수 있다.
+
+   <br>
+
+## Observable
+Observable 은 데이터가 흐르는 Stream 이다.  
+Observable 에서 이벤트(값) 을 방출할 때는 상태를 함께 방출한다.
+
+ - `onNext` : 정상적인 데이터 방출
+ - `onError` : 에러 방출
+ - `onCompleted` : 옵저버블의 종료 방출
+   
+   <br>
+
+ 기본적인 Observable 의 생성: Observable.create() 
+ 
+<br>
+
+## Single
+옵저버블 중에서, 오직 하나의 값만을 방출하는 옵저버블
+
+- `onSuccess`: 옵저버블의 onNext 와 동일
+- `onFailure`: 옵저버블의 onError 와 동일
+
+   <br>
+
+## Subject
+옵저버블 중에서 외부에서 값을 넣어줄 수 있는 옵저버블
+
+- **`BehaviorSubject` : 초깃값이 있는 Subject. 구독시 가장 최근에 방출된 값을 받음.**
+
+- **`PublishSubject` : 초깃값이 없는 Subject. 구독 이후로 방출된 값을 받음.**
--- RxSwiftStudy/RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/ViewController.swift
@@ -10,11 +10,89 @@ import RxSwift
 
 class ViewController: UIViewController {
 
+    // 구독 해제 위한 가방
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        test5()
     }
 
-
+    // MARK: - Observable
+    func test() {
+        // 옵져버블 생성
+        let newObservable: Observable<String> = Observable.create { observer in
+            // 순서대로 "Adam", "Eve", "Abel" 이라는 값을 방출하도록
+            observer.onNext("Adam")
+            observer.onNext("Eve")
+            observer.onNext("Abel")
+            
+            // Disposables.create() 를 작성함으로써 옵저버블 생성 완료
+            return Disposables.create()
+        }
+        
+        // 구독
+        newObservable.subscribe(onNext: { value in
+            print("이름: \(value)")
+        }).disposed(by: disposeBag) // observable 이 종료되었을 때 구독을 해제할 disposeBag
+    }
+    
+    func test2() {
+        // 1초마다 값을 방출하는 옵져버블
+        let someObservable = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance).take(5)
+        // --> 정수형 데이터 방출하는 옵져버블, 1초마다 방출, scheduler: 스레드 의미 / 메인 스레드에서 방출, 5번만 방출
+        // RxSwift 다양한 연산자, 메서드 제공
+        
+        someObservable.subscribe(onNext: { value in
+            print("방출된 값: \(value)")
+        }, onCompleted: {
+            print("Completed")
+        }).disposed(by: disposeBag)
+        
+        print("Hello")
+        // --> 비동기적 수행으로, Hello 먼저 찍히게 됨.
+        // RxSwift 는 옵져버 패턴과 비동기 프로그래밍에 도움을 줄 수 있다 !
+    }
+    
+    // MARK: - Single
+    func test3() {
+        // Single 생성
+        let single = Single.create { observer in
+            observer(.success("Adam"))
+            return Disposables.create()
+        }
+        
+        single.subscribe(onSuccess: { value in
+            print(value)
+        }).disposed(by: disposeBag)
+    }
+    
+    // MARK: - BehaviorSubject
+    func test4() {
+        // BehaviorSubject 생성
+        let subject = BehaviorSubject(value: 10)
+        
+        subject.subscribe(onNext: { value in
+            print("값 방출: \(value)")
+        }).disposed(by: disposeBag)
+        
+        // 외부에서 값 넣어줌
+        subject.onNext(20)
+        subject.onNext(30)
+    }
+    // MARK: - PublishSubject
+    func test5() {
+        // PublishSubject 생성
+        let subject = PublishSubject<Int>()
+        
+        subject.subscribe(onNext: { value in
+            print("값 방출: \(value)")
+        }).disposed(by: disposeBag)
+        
+        subject.onNext(20)
+        subject.onNext(30)
+    }
+    
 }
 
